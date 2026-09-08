1d78aad3f781ed85ffea55a00ab8c7af2e0d7a0a
Mc. Colly <collybalagtas123@gmail.com>
2026-09-09 00:04:28 +0800

    ✅ Java Stdin and Stdout II [java] — approach1

---
+++ solutions/java/easy/java-stdin-stdout/solution-approach1.java
@@ -0,0 +1,30 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-stdin-stdout/problem?isFullScreen=true
+// Problem     Java Stdin and Stdout II
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-09, 12:04 a.m.
+// ──────────────────────────────────────────────────
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int i = scan.nextInt(); 
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        
+        scan.close();
+        
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
