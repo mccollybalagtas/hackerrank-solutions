9fb656818d839f0ae88489c183a7d0f2511f85fc
Mc. Colly <collybalagtas123@gmail.com>
2026-09-08 18:57:20 +0800

    ✅  Java Stdin and Stdout I [java] — approach1

---
+++ solutions/java/easy/java-stdin-and-stdout-1/solution-approach1.java
@@ -0,0 +1,28 @@
+// ──────────────────────────────────────────────────
+// Link        https://www.hackerrank.com/challenges/java-stdin-and-stdout-1/problem?isFullScreen=true
+// Problem      Java Stdin and Stdout I
+// Difficulty  Easy
+// Subdomain   Introduction
+// Platform    HackerRank
+// Language    java
+// Status      Accepted
+// Submitted   2026-09-08, 06:57 p.m.
+// ──────────────────────────────────────────────────
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();   
+        int b = scan.nextInt();  
+        int c = scan.nextInt();   
+
+        scan.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
